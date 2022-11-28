89ea7dc15b3095badb80e5010db7b7e12acf5bd5
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-28 16:24:46 +0700

    Add project files.

---
+++ StackUsingSingleLinkList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+    class Stack
+    {
+        node top;
+        public Stack()
+        {
+            top = null;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
