9c4b48cb34285a43c1e6aeb832baf3faa495bdca
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-28 16:44:49 +0700

    ...

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -53,19 +53,24 @@ namespace StackUsingSingleLinkList
             {
                 for (tmp = top; tmp != null; tmp = tmp.next)
                 {
-
+                    Console.WriteLine(tmp.info);
                 }
             }
         }
-    }
-    
-
-
-    }
-
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice:");
+            }
         }
     }
+}
+    
