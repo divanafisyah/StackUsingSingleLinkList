c08b43e0019207b941cc9978952f0198c4e29029
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-28 16:38:42 +0700

    add method display

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -41,7 +41,21 @@ namespace StackUsingSingleLinkList
         }
         public void pop()
         {
+            Console.WriteLine("\n The poped elements is: " + top.info);
+            top = top.next; //make top point to the next node is squence
+        }
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
 
+                }
+            }
         }
     }
     
